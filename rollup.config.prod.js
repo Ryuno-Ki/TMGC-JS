da17e73654a1572693bef18101eb9c1f537b2aac
André Jaenisch <andre.jaenisch@posteo.de>
2017-12-12 22:16:41 +0100

    Fixed rollup.config.prod

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,36 +2,15 @@ import { join } from 'path'
 
 import commonjs from 'rollup-plugin-commonjs'
 import copy from 'rollup-plugin-copy'
-import eslint from 'rollup-plugin-eslint'
-import coverage from 'rollup-plugin-istanbul'
-import livereload from 'rollup-plugin-livereload'
 import builtins from 'rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
 import resolve from 'rollup-plugin-node-resolve'
 import scss from 'rollup-plugin-scss'
-import serve from 'rollup-plugin-serve'
 import url from 'rollup-plugin-url'
 
 const copyOptions = {
   'src/index.html': 'dist/index.html',
   'src/img/chars/00/hatch.gif': 'dist/img/chars/00/hatch.gif',
-}
-
-const coverageOptions = {
-  exclude: [
-    'test/*.js',
-  ]
-}
-
-const eslintOptions = {
-  exclude: [
-    'node_modules/**',
-    'src/img/**'
-  ]
-}
-
-const livereloadOptions = {
-  watch: 'dist'
 }
 
 const urlOptions = {
@@ -46,11 +25,6 @@ const scssOptions = {
   output: 'dist/app.css'
 }
 
-const serveOptions = {
-  contentBase: 'dist',
-  port: 8080,
-}
-
 const plugins = [
   copy(copyOptions),
   scss(scssOptions),
@@ -58,14 +32,10 @@ const plugins = [
   builtins(),
   resolve(resolveOptions),
   commonjs(),
-  eslint(eslintOptions),
-  coverage(coverageOptions),
   url(urlOptions),
-  serve(serveOptions),
-  livereload(livereloadOptions),
 ]
 
-export function getDevConfig() {
+function getProdConfig() {
  return {
     input: join(__dirname, 'src', 'index.js'),
     output: {
@@ -76,3 +46,5 @@ export function getDevConfig() {
     plugins: plugins
   }
 }
+
+export default getProdConfig()
